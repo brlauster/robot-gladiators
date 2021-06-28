b75585281c7ed247c912af20053157c5c3736d7c
Brandon Lauster <brandon@Brandons-MacBook-Air.local>
2021-06-28 19:59:10 -0400

    add java

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,18 +1,44 @@
-var playerName = window.prompt ("What is your robot's name?")
+var playerName = window.prompt("What is your robot's name?");
+var playerHealth = 100;
+var playerAttack = 10;
 
-console.log(playerName);
+// You can also log multiple values at once like this
+console.log(playerName, playerAttack, playerHealth);
 
-console.log("This logs a string, good for leaving yourself a message");
+// put new code under this
+console.log(
+  playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+);
 
-console.log(10 + 10);
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
 
-console.log("Our robot's name is " + playerName);
+var fight = function() {
+   // Alert players that they are starting the round
+   window.alert("Welcome to Robot Gladiators!");
 
-var name - "Brandon Lauster"
-console.log(name)
+   //Subtract the value of `playerAttack` from the value of `enemyHealth` and use that result to update the value in the `enemyHealth` variable
+   enemyHealth = enemyHealth - playerAttack;
 
-function fight() {
-    window.alert("The fight has begun!");
+   // Log a resulting message to the console so we know that it worked.
+   playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+
+   // Subtract the value of `enemyAttack` from the value of `playerHealth` and use that result to update the value in the `playerHealth` variable.
+   playerHealth = playerHealth - enemyAttack;
+
+   // Log a resulting message to the console so we know that it worked.
+   console.log(
+    enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+  );
+
+  // check player's health
+  if (playerHealth <= 0) {
+  window.alert(playerName + " has died!");
+  } 
+  else {
+  window.alert(playerName + " still has " + playerHealth + " health left.");
   }
+  };
 
-// fight();
+fight();
